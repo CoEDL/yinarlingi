f4391e0df7d589441df7e990c125d22e8c90d2c0
Nay San <nay.san@anu.edu.au>
2018-07-11 14:07:42 +1000

    test_code1_ordered returns prettified results by default (shows only the +/- 3 lines adjacent of the failing line)

--- R/test_code1_ordered.R
+++ R/test_code1_ordered.R
@@ -4,6 +4,8 @@
 #' @param return_all return the all from data frame or just entries with errors (default is \code{FALSE})
 #' @param grammar_file a Nearley grammar file (default is `wlp_skeleton-simple.ne`)
 #'
+#' @importFrom purrr map_int
+#'
 #' @export
 #'
 
@@ -27,7 +29,24 @@ test_code1_ordered <- function(wlp_lexicon, return_all = FALSE, grammar_file = s
 
     if(!return_all) {
         result_df %>%
-            filter(any(!code1_ok) | any(is.na(code1_ok)))
+            filter(any(!code1_ok) | any(is.na(code1_ok))) %>%
+
+            # Find out which line is failing, and get the 3 lines on either side of the failing line
+            group_by(me_start) %>%
+            nest() %>%
+            mutate(failing_line = map_int(data, ~ filter(.,code1_ok == FALSE | is.na(code1_ok)) %>% slice(1) %>% pull(line))) %>%
+            unnest(data,.drop = FALSE) %>%
+            filter(line >= failing_line - 3, line <= failing_line + 3) %>%
+            ungroup %>%
+            mutate(
+                me_start = ifelse(duplicated(me_start), "", me_start),
+                error    = case_when(
+                    code1_ok == FALSE                       ~ "Unexpected code1",
+                    is.na(code1_ok) & lag(code1_ok) == TRUE ~ "Parse incomplete, expecting another code1 after."
+                )
+            ) %>%
+            select(-failing_line)
+
     } else {
         result_df
     }
