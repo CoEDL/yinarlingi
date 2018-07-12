e983cdb055e12bcdb6130e0c3508129eeaf606d3
Nay San <nay.san@anu.edu.au>
2018-07-12 12:25:55 +1000

    Fixed test failing when there are no lexicographical errors (error table prettifier code doesn't work when there are no errors to report)!

--- R/test_code1_ordered.R
+++ R/test_code1_ordered.R
@@ -28,25 +28,29 @@ test_code1_ordered <- function(wlp_lexicon, return_all = FALSE, grammar_file = s
         mutate(code1_ok = entry_parser$parse_str(code1, return_labels = TRUE))
 
     if(!return_all) {
-        result_df %>%
-            filter(any(!code1_ok) | any(is.na(code1_ok))) %>%
+        errors_df <- result_df %>%
+            filter(any(!code1_ok) | any(is.na(code1_ok)))
 
+        if(nrow(errors_df) == 0) {
+            errors_df
+        } else {
             # Find out which line is failing, and get the 3 lines on either side of the failing line
-            group_by(me_start) %>%
-            nest() %>%
-            mutate(failing_line = map_int(data, ~ filter(.,code1_ok == FALSE | is.na(code1_ok)) %>% slice(1) %>% pull(line))) %>%
-            unnest(data,.drop = FALSE) %>%
-            filter(line >= failing_line - 3, line <= failing_line + 3) %>%
-            ungroup %>%
-            mutate(
-                me_start = ifelse(duplicated(me_start), "", me_start),
-                error    = case_when(
-                    code1_ok == FALSE                       ~ "Unexpected code1",
-                    is.na(code1_ok) & lag(code1_ok) == TRUE ~ "Parse incomplete, expecting another code1 after."
-                )
-            ) %>%
-            select(-failing_line)
-
+            errors_df %>%
+                group_by(me_start) %>%
+                nest() %>%
+                mutate(failing_line = map_int(data, ~ filter(.,code1_ok == FALSE | is.na(code1_ok)) %>% slice(1) %>% pull(line))) %>%
+                unnest(data,.drop = FALSE) %>%
+                filter(line >= failing_line - 3, line <= failing_line + 3) %>%
+                ungroup %>%
+                mutate(
+                    me_start = ifelse(duplicated(me_start), "", me_start),
+                    error    = case_when(
+                        code1_ok == FALSE                       ~ "Unexpected code1",
+                        is.na(code1_ok) & lag(code1_ok) == TRUE ~ "Parse incomplete, expecting another code1 after."
+                    )
+                ) %>%
+                select(-failing_line)
+        }
     } else {
         result_df
     }
